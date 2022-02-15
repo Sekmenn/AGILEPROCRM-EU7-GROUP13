122eee0ab9a15d629e90bc98a2ea7358569b579d
Sekmenn <omerfaruksekmenn@gmail.com>
2022-02-15 21:43:50 +0100

    Added something

--- src/test/java/com/agileprocrm/pages/HelloGuys.java
+++ src/test/java/com/agileprocrm/pages/HelloGuys.java
@@ -7,6 +7,7 @@ public class HelloGuys {
         System.out.println("Hi Guys!!!");
         System.out.println("Hi Guys!!!");
         System.out.println("Doing something good or bad");
+        System.out.println("Adding something");
 
     }
 
